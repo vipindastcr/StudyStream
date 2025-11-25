1a277e1efaf60b0ba7ec128633cbb033d16e096e
vipindastcr <vpn9995@gmail.com>
2025-11-25 15:54:26 +0530

    FIX: otp DONE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Register from "@/pages/Register";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position="top-center" />
       <Routes>
         <Route path="/" element={<Register />} />
+        <Route path="/login" element={<div style={{padding:24, textAlign:'center'}}>Login Page</div>} />
       </Routes>
     </BrowserRouter>
   );
